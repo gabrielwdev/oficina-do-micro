0f31d542ad7f3379a7ec859231c36f75417e7386
pgabriel30 <paulogabriel.bsm@gmail.com>
2023-06-28 10:28:26 -0300

    teste mobile

--- src/assets/components/Footer.jsx
+++ src/assets/components/Footer.jsx
@@ -1,8 +1,10 @@
+import Logo from "./img/logo-oficina-do-micro.png";
+
 export default function Footer() {
   return (
     <footer class="footer">
       <nav class="footer-navbar">
-        <img src="" alt="Logo da Hytech Vale" class="footer-navbar-logo" />
+        <img src={Logo} alt="Logo da Hytech Vale" class="footer-navbar-logo" />
         <div class="footer-navbar-complementos">
           <p>
             © 2023 Oficina do Micro. Todos os direitos reservados | Site
@@ -18,7 +20,16 @@ export default function Footer() {
             </a>
           </address>
         </div>
-        <a href="#top" class="footer-navbar-link"></a>
+        <a href="#top" class="footer-navbar-link">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor"
+            class="bi bi-arrow-up-circle-fill"
+            viewBox="0 0 16 16"
+          >
+            <path d="M16 8A8 8 0 1 0 0 8a8 8 0 0 0 16 0zm-7.5 3.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707V11.5z" />
+          </svg>
+        </a>
       </nav>
     </footer>
   );
