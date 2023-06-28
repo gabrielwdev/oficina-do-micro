3cba626d54986752b1e3122753d15ded0dff36e2
pgabriel30 <paulogabriel.bsm@gmail.com>
2023-06-28 11:27:33 -0300

    teste mobile 2

--- src/assets/components/StnProcessos.jsx
+++ src/assets/components/StnProcessos.jsx
@@ -5,50 +5,58 @@ export default function StnProcessos() {
       <ul className="processos-lista">
         <li className="processos-lista-card">
           <div className="processos-lista-card-circulo">1°</div>
-          <div className="div processos-lista-card-conteudo">
-            <h3 className="div processos-lista-card-conteudo-titulo">
-              Inspeção e diagnóstico
-            </h3>
-            <p className="div processos-lista-card-conteudo-descricao">
-              Realizamos os testes em bancada para encontrar o defeito e
-              elaborar o orçamento.
-            </p>
+          <div className="processos-lista-card-container">
+            <div className="div processos-lista-card-container-conteudo">
+              <h3 className="div processos-lista-card-container-conteudo-titulo">
+                Inspeção e diagnóstico
+              </h3>
+              <p className="div processos-lista-card-container-conteudo-descricao">
+                Realizamos os testes em bancada para encontrar o defeito e
+                elaborar o orçamento.
+              </p>
+            </div>
           </div>
         </li>
         <li className="processos-lista-card">
           <div className="processos-lista-card-circulo">2°</div>
-          <div className="div processos-lista-card-conteudo">
-            <h3 className="div processos-lista-card-conteudo-titulo">
-              Reparo necessário
-            </h3>
-            <p className="div processos-lista-card-conteudo-descricao">
-              Após aprovação do orçamento iniciamos o reparo, substituindo
-              somente as peças danificadas.
-            </p>
+          <div className="processos-lista-card-container">
+            <div className="div processos-lista-card-container-conteudo">
+              <h3 className="div processos-lista-card-container-conteudo-titulo">
+                Reparo necessário
+              </h3>
+              <p className="div processos-lista-card-container-conteudo-descricao">
+                Após aprovação do orçamento iniciamos o reparo, substituindo
+                somente as peças danificadas.
+              </p>
+            </div>
           </div>
         </li>
         <li className="processos-lista-card">
           <div className="processos-lista-card-circulo">3°</div>
-          <div className="div processos-lista-card-conteudo">
-            <h3 className="div processos-lista-card-conteudo-titulo">
-              Limpeza completa
-            </h3>
-            <p className="div processos-lista-card-conteudo-descricao">
-              Terminando o reparo, o equipamento passa por uma limpeza, para
-              eliminar qualquer tipo de e riscos de problemas futuros.
-            </p>
+          <div className="processos-lista-card-container">
+            <div className="div processos-lista-card-container-conteudo">
+              <h3 className="div processos-lista-card-container-conteudo-titulo">
+                Limpeza completa
+              </h3>
+              <p className="div processos-lista-card-container-conteudo-descricao">
+                Terminando o reparo, o equipamento passa por uma limpeza, para
+                eliminar qualquer tipo de e riscos de problemas futuros.
+              </p>
+            </div>
           </div>
         </li>
         <li className="processos-lista-card">
           <div className="processos-lista-card-circulo">4°</div>
-          <div className="div processos-lista-card-conteudo">
-            <h3 className="div processos-lista-card-conteudo-titulo">
-              Testes e controle de qualidade
-            </h3>
-            <p className="div processos-lista-card-conteudo-descricao">
-              Realizaremos diversos testes de usabilidade, afim de garantir que
-              o problema tenha sido 100% resolvido
-            </p>
+          <div className="processos-lista-card-container">
+            <div className="div processos-lista-card-container-conteudo">
+              <h3 className="div processos-lista-card-container-conteudo-titulo">
+                Testes e controle de qualidade
+              </h3>
+              <p className="div processos-lista-card-container-conteudo-descricao">
+                Realizaremos diversos testes de usabilidade, afim de garantir
+                que o problema tenha sido 100% resolvido
+              </p>
+            </div>
           </div>
         </li>
       </ul>
